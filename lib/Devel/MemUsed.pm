9b401dcf161832541afecc89a39298461940ccd2
jk <jk@b0c1b02c-7ba7-4df7-b273-855bf36df2ab>
2008-09-11 16:02:07 +0000

    playing arround :-)
    git-svn-id: https://cle.sk/repos/pub/cpan/Devel-MemUsed/trunk@647 b0c1b02c-7ba7-4df7-b273-855bf36df2ab

--- lib/Devel/MemUsed.pm
+++ lib/Devel/MemUsed.pm
@@ -2,7 +2,22 @@ package Devel::MemUsed;
 
 =head1 NAME
 
+Devel::MemUsed - returns how much memory as allocated since the Devel::MemUsed object construction
+
 =head1 SYNOPSIS
+
+    use Devel::MemUsed;
+    
+    my $memused = Devel::MemUsed->new();
+    my %h = ( map { $_ => 1 } (1..100) );
+    print "my hash allocated $memused bytes of memory\n";
+    # for me 15632
+    
+    $memused->reset;
+    
+    my %h = ( map { $_ => 1 } (1..1000) );
+    print "my hash allocated $memused bytes of memory\n";
+    # for me 128104
 
 =head1 DESCRIPTION
 
@@ -13,11 +28,17 @@ use strict;
 
 our $VERSION = '0.01';
 
+use base 'Class::Accessor::Fast';
+use Contextual::Return;
+use Devel::Mallinfo 'mallinfo';
+
 =head1 PROPERTIES
 
-no properties ;-)
-
 =cut
+
+__PACKAGE__->mk_accessors(qw{
+    memory_offset
+});
 
 =head1 METHODS
 
@@ -29,14 +50,34 @@ Object constructor.
 
 sub new {
     my $class = shift;
-    my $self  = {};
+    my $self  = $class->SUPER::new({ @_ });
     
-    bless $self, __PACKAGE__;
+    $self->reset;
     
-    return $self;
+    return ACTIVE
+        OBJREF   { $self }
+        DEFAULT  { $self->used }
+    ;
 }
 
-1;
+sub used {
+    my $self = shift;    
+    return $self->allocated_memory - $self->memory_offset;
+}
+
+sub reset {
+    my $self = shift;
+    return $self->memory_offset($self->allocated_memory);
+}
+
+sub allocated_memory {
+    my $self = shift;
+    
+    my $m = mallinfo();
+    return $m->{'uordblks'} + $m->{'hblkhd'};
+}
+
+'always a lot, what do you think?';
 
 
 __END__
--- t/01_Devel-MemUsed.t
+++ t/01_Devel-MemUsed.t
@@ -3,8 +3,8 @@
 use strict;
 use warnings;
 
-use Test::More 'no_plan';
-#use Test::More tests => 10;
+#use Test::More 'no_plan';
+use Test::More tests => 8;
 
 use FindBin qw($Bin);
 use lib "$Bin/lib";
@@ -17,9 +17,29 @@ exit main();
 
 sub main {
 	my $memused = Devel::MemUsed->new();
-	isa_ok($memused, 'Devel::MemUsed');
+	ok($memused->isa('Devel::MemUsed'), 'isa Devel::MemUsed');
 	
+	cmp_ok($memused, '<', 5*1024, 'not a lot of memory should be allocated so far');
 	
+	$memused->reset;
+	
+#	my $x = "x" x (5*1024);           # this one takes >10kB of memory ?!?!?
+	my $x = eval '"x" x (5*1024)';    # this one just   ~5kB
+	cmp_ok($memused, '>', 5*1024, 'more then 5kB should be allocated ('.$memused.')');
+	cmp_ok($memused, '<', 10*1024, 'but less then 10kB should be allocated ('.$memused.')');
+	
+	$memused->reset;
+
+#	my $x2 = "x" x (100*1024);           # this one takes >200kB of memory ?!?!?
+	my $x2 = eval '"x" x (100*1024)';    # this one just   ~100kB
+	cmp_ok($memused, '>', 100*1024, 'more then 100kB should be allocated now ('.$memused.')');
+	cmp_ok($memused, '<', 110*1024,  'but less then 110kB should be allocated ('.$memused.')');
+
+	$memused->reset;
+
+	# 127*1024 is a magic border when hblkhd start to increase		
+	my $x3 = eval '"x" x (128*1024)';
+	cmp_ok($memused, '>', 128*1024, 'more then 128kB should be allocated now ('.$memused.')');
+
 	return 0;
 }
-
